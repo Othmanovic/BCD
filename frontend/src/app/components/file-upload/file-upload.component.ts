9b8660f94d1accb7fc6006b4ee3c7fa32eb6fb72
Othman Alhadiri <othman@wavelet.net>
2023-08-26 03:20:14 +0800

    showing loading when file is being uploaded

--- frontend/src/app/components/file-upload/file-upload.component.ts
+++ frontend/src/app/components/file-upload/file-upload.component.ts
@@ -9,128 +9,147 @@ import { FileService } from "src/app/services/file-upload/file.service";
   styleUrls: ["./file-upload.component.scss"]
 })
 export class FileUplaodComponent implements OnInit {
-  selectedFile: File;
+  selectedFile1: File;
+  selectedFile2: File;
+  selectedFile3: File;
 
-  file1: File;
-  file2: File;
-  file3: File;
+  isUploading1: boolean = false;
+  isUploading2: boolean = false;
+  isUploading3: boolean = false;
 
   constructor(
     private http: HttpClient,
     private fileService: FileService,
-    ) { }
+  ) { }
 
-  onFileUpload(event: any) {
+  onFileUpload1(event: any) {
     const file = event.target.files[0];
     const allowedExtensions = /(\.txt)$/i;
 
     if (allowedExtensions.test(file.name)) {
-      this.selectedFile = file;
+      this.selectedFile1 = file;
     } else {
       // Show popup or alert with error message
       alert('Invalid file type. Please select a .txt file.');
       // Clear the file input value
       event.target.value = '';
       // Reset the selected file
-      this.selectedFile = null;
+      this.selectedFile1 = null;
     }
   }
 
-  onFileChange(event: any) {
-    this.selectedFile = event.target.files[0] as File;
+  onFileUpload2(event: any) {
+    const file = event.target.files[0];
+    const allowedExtensions = /(\.txt)$/i;
+
+    if (allowedExtensions.test(file.name)) {
+      this.selectedFile2 = file;
+    } else {
+      // Show popup or alert with error message
+      alert('Invalid file type. Please select a .txt file.');
+      // Clear the file input value
+      event.target.value = '';
+      // Reset the selected file
+      this.selectedFile2 = null;
+    }
   }
 
-  uploadFile() {
-    if (!this.selectedFile) {
+  onFileUpload3(event: any) {
+    const file = event.target.files[0];
+    const allowedExtensions = /(\.txt)$/i;
+
+    if (allowedExtensions.test(file.name)) {
+      this.selectedFile3 = file;
+    } else {
+      // Show popup or alert with error message
+      alert('Invalid file type. Please select a .txt file.');
+      // Clear the file input value
+      event.target.value = '';
+      // Reset the selected file
+      this.selectedFile3 = null;
+    }
+  }
+
+
+
+  uploadFile1() {
+    if (!this.selectedFile1) {
       alert('Please select a file first.');
       return;
     }
 
     // Check if the selected file is a text file
-    if (this.selectedFile.type !== 'text/plain') {
+    if (this.selectedFile1.type !== 'text/plain') {
       alert('Please select a .txt file.');
       return;
     }
 
-    this.fileService.uploadFile(this.selectedFile).subscribe(
+    this.isUploading1 = true;
+
+    this.fileService.uploadFile(this.selectedFile1).subscribe(
       (response) => {
         console.log('File uploaded successfully!', response);
+        this.isUploading1 = false;
       },
       (error) => {
         console.error('Error uploading file:', error);
+        this.isUploading1 = false;
       }
     );
   }
 
   uploadFile2() {
-    if (!this.selectedFile) {
+    if (!this.selectedFile2) {
       alert('Please select a file first.');
       return;
     }
 
     // Check if the selected file is a text file
-    if (this.selectedFile.type !== 'text/plain') {
+    if (this.selectedFile2.type !== 'text/plain') {
       alert('Please select a .txt file.');
       return;
     }
 
-    this.fileService.uploadFile2(this.selectedFile).subscribe(
+    this.isUploading2 = true;
+
+    this.fileService.uploadFile2(this.selectedFile2).subscribe(
       (response) => {
         console.log('File uploaded successfully!', response);
+        this.isUploading2 = false;
       },
       (error) => {
         console.error('Error uploading file:', error);
+        this.isUploading2 = false;
       }
     );
   }
 
   uploadFile3() {
-    if (!this.selectedFile) {
+    if (!this.selectedFile3) {
       alert('Please select a file first.');
       return;
     }
 
     // Check if the selected file is a text file
-    if (this.selectedFile.type !== 'text/plain') {
+    if (this.selectedFile3.type !== 'text/plain') {
       alert('Please select a .txt file.');
       return;
     }
 
-    this.fileService.uploadFile3(this.selectedFile).subscribe(
+    this.isUploading3 = true;
+
+    this.fileService.uploadFile3(this.selectedFile3).subscribe(
       (response) => {
         console.log('File uploaded successfully!', response);
+        this.isUploading3 = true;
       },
       (error) => {
         console.error('Error uploading file:', error);
+        this.isUploading3 = true;
       }
     );
   }
-  
-  // onFileSelected(event: any, fileNumber: number) {
-  //   const file: File = event.target.files[0];
-  //   switch (fileNumber) {
-  //     case 1:
-  //       this.file1 = file;
-  //       break;
-  //     case 2:
-  //       this.file2 = file;
-  //       break;
-  //     case 3:
-  //       this.file3 = file;
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  // }
 
-
-  // uploadFile() {
-  //   // Perform upload logic here
-  //   console.log('Uploading file:', this.selectedFile);
-  //   // Reset the form after upload
-  //   this.selectedFileType = null;
-  //   this.selectedFile = null;
-  // }
 
   ngOnInit() { }
 
